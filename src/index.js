7dc590f065136c5d84405ddb16da33709ce9fa05
johneckert <johnteckert@gmail.com>
2018-06-12 14:53:45 -0400

    added basic file structure/redux/router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import store from './store/configureStore';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
---
+++ src/reducers/citiesReducer.js
@@ -0,0 +1,9 @@
+import initialState from './initialState';
+import {} from '../actions/actionTypes';
+
+export default function cities(state = initialState.cities, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
