b2455ee4ff4128dd316f9861d22cf9cec9a9310e
johneckert <johnteckert@gmail.com>
2018-06-12 21:44:05 -0400

    restructured initial state - removed currentObj key

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,7 +1,6 @@
 const initialState = {
   cities: ['Copenhagen', 'Tokyo', 'Delhi', 'New York', 'Istanbul'], //making this dynamic would allow to show any cities
   weatherObjs: [],
-  relevantWeatherObj: {},
   forecastObj: {},
   units: 'f'
 };
