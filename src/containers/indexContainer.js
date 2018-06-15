c7376940abca2f6805f4f583775346112d1b7c7b
johneckert <johnteckert@gmail.com>
2018-06-15 14:22:29 -0400

    added loading component and fixed bad svgs

--- src/containers/indexContainer.js
+++ src/containers/indexContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CityListItem from '../components/cityListItem';
+import Loader from '../components/loader';
 
 class IndexContainer extends Component {
   sortedCities = () => {
@@ -17,15 +18,15 @@ class IndexContainer extends Component {
   };
 
   render() {
-    return this.props.weatherObjs.length === 5 ? ( //change this number if more cities are added
+    return (
       <div className="main">
-        {this.sortedCities().map(weatherObj => {
-          return <CityListItem key={weatherObj.id} weatherObj={weatherObj} />;
-        })}
-      </div>
-    ) : (
-      <div className="main">
-        <h2>LOADING...</h2>
+        {this.props.weatherObjs.length === 5 ? ( //change this number if more cities are added
+          this.sortedCities().map(weatherObj => {
+            return <CityListItem key={weatherObj.id} weatherObj={weatherObj} />;
+          })
+        ) : (
+          <Loader />
+        )}
       </div>
     );
   }
