91f8f179c760b1fba5d1d6e6fd5c9d71ee57acd9
johneckert <johnteckert@gmail.com>
2018-06-15 14:09:58 -0400

    cities render alphabatized

--- src/containers/indexContainer.js
+++ src/containers/indexContainer.js
@@ -3,10 +3,23 @@ import { connect } from 'react-redux';
 import CityListItem from '../components/cityListItem';
 
 class IndexContainer extends Component {
+  sortedCities = () => {
+    const sorted = this.props.weatherObjs.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+    return sorted;
+  };
+
   render() {
-    return this.props.weatherObjs.length > 0 ? (
+    return this.props.weatherObjs.length === 5 ? (
       <div className="main">
-        {this.props.weatherObjs.map(weatherObj => {
+        {this.sortedCities().map(weatherObj => {
           return <CityListItem key={weatherObj.id} weatherObj={weatherObj} />;
         })}
       </div>
