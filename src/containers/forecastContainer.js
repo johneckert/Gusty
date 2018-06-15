dd584401f20568667c60bb48bdd202309dc00c03
johneckert <johnteckert@gmail.com>
2018-06-15 19:20:56 -0400

    changed forecast container scroll to buttons

--- src/containers/forecastContainer.js
+++ src/containers/forecastContainer.js
@@ -1,32 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Day from '../components/day';
 
-const ForecastContainer = props => {
-  return (
-    <div className="forecast-panel">
-      <h2>5 Day Forecast</h2>
-      {props.forecastObj.error ? (
-        <p className="error">Forecast Data Unavailable</p>
-      ) : (
-        <ul>
-          {props.forecastObj ? (
-            Object.values(props.forecastObj).map(dayArr => {
-              return (
-                <li key={dayArr[0].day}>
-                  <Day dayArr={dayArr} />
-                </li>
-              );
-            })
-          ) : (
-            <li>--</li>
-          )}
-        </ul>
-      )}
-      <img className="arrow" src="./chevron-right.svg" alt=">" />
-    </div>
-  );
-};
+class ForecastContainer extends Component {
+  handleRightClick = () => {
+    this.refs.scroller.scrollLeft += 200;
+  };
+
+  handleLeftClick = () => {
+    this.refs.scroller.scrollLeft -= 200;
+  };
+
+  renderError = () => <p className="error">Forecast Data Unavailable</p>;
+
+  render() {
+    return (
+      <div className="forecast-panel">
+        <div className="arrow-flipped" onClick={this.handleLeftClick}>
+          <img src="./chevron-right.svg" alt=">" />
+        </div>
+        <h2>5 Day Forecast</h2>
+        {this.props.forecastObj.error ? (
+          this.renderError()
+        ) : (
+          <ul ref="scroller">
+            {this.props.forecastObj ? (
+              Object.values(this.props.forecastObj).map(dayArr => {
+                return (
+                  <li key={dayArr[0].day}>
+                    <Day dayArr={dayArr} />
+                  </li>
+                );
+              })
+            ) : (
+              <li>--</li>
+            )}
+          </ul>
+        )}
+        <div className="arrow" onClick={this.handleRightClick}>
+          <img src="./chevron-right.svg" alt=">" />
+        </div>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => {
   return { forecastObj: state.forecastObj };
