64c63e32882d0602a30fbb693c7e5ae00c0773e8
johneckert <johnteckert@gmail.com>
2018-06-15 22:08:31 -0400

    config var problem

--- src/services/forecastDataAPI.js
+++ src/services/forecastDataAPI.js
@@ -1,9 +1,11 @@
 import BASE_URL from './URL';
 
+//normally I would hide the API key, but I was having difficulty with heroku's config vars for some reason
+
 class ForecastData {
   static getForecast(cityName) {
     return fetch(
-      `${BASE_URL}forecast?q=${cityName}&APPID=${process.env.APP_ID}&units=imperial`
+      `${BASE_URL}forecast?q=${cityName}&APPID=c49db1b5fc6a2f4f7a4dcc5c435bf737&units=imperial`
     ).then(res => res.json());
   }
 }
