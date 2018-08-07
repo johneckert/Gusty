b1de49ea7cc168ce0cb67c2643f4fc5159c2235a
John Eckert <johnteckert@gmail.com>
2018-08-07 11:14:24 -0400

    Updates

--- src/services/URL.js
+++
@@ -1,3 +0,0 @@
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
-
-export default BASE_URL;
--- src/store/configureStore.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import weatherReducer from '../reducers/weatherReducer';
-import thunk from 'redux-thunk';
-
-const store = createStore(weatherReducer, applyMiddleware(thunk));
-
-export default store;
